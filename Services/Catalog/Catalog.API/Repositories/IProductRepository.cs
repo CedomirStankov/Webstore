3864c8c8507ebc9c41bbc01f712a501e99267e1f
Cedomir <cstankov3921rn@raf.rs>
2025-07-17 13:01:56 +0200

    added the rest of db operations for products

--- Services/Catalog/Catalog.API/Repositories/IProductRepository.cs
+++ Services/Catalog/Catalog.API/Repositories/IProductRepository.cs
@@ -5,5 +5,10 @@ namespace Catalog.API.Repositories
     public interface IProductRepository
     {
         Task<IEnumerable<Product>> GetProducts();
+        Task<Product> GetProduct(string id);
+        Task<IEnumerable<Product>> GetProductsByCategory(string categoryName);
+        Task CreateProduct(Product product);
+        Task<bool> UpdateProduct(Product product);
+        Task<bool> DeleteProduct(string id);
     }
 }
